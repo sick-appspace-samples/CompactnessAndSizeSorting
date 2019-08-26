d77ea85d3f63e227292f81520d17873df02addc3
Florian Roser <Florian.Roser@sick.de>
2019-08-26 18:34:40 +0200

    'Update'

--- CompactnessAndSizeSorting/scripts/CompactnessAndSizeSorting.lua
+++ CompactnessAndSizeSorting/scripts/CompactnessAndSizeSorting.lua
@@ -8,8 +8,8 @@
   
   How to Run:
   Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
+  debugging (F7+F10). Setting a breakpoint on the first row inside the 'main'
+  function allows debugging step-by-step after the 'Engine.OnStarted' event.
   Results can be seen in the image viewer on the DevicePage.
   Restarting the Sample may be necessary to show images after loading the webpage.
   To run this Sample a device with SICK Algorithm API and AppEngine >= V2.5.0 is
@@ -29,7 +29,6 @@ local DELAY = 1000
 
 -- Creating viewer
 local viewer = View.create()
-viewer:setID('viewer2D')
 
 -- Setting up graphical overlay attributes
 local dotDecoration = View.ShapeDecoration.create()
@@ -88,6 +87,7 @@ local function main()
   print(#selectedBlobs .. ' blobs selected')
   print('App finished.')
 end
+
 --The following registration is part of the global scope which runs once after startup
 --Registration of the 'main' function to the 'Engine.OnStarted' event
 Script.register('Engine.OnStarted', main)
